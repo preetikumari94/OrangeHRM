071b365b9c0ae418ef95ac77cef3c864435a9d1c
preetikumari94 <pree0717@gmail.com>
2024-10-17 22:51:08 +0530

    Add files via upload
    Adding new file

---
+++ OrangeHRM/src/test/java/OrangeHRMTestCase/TestCases.java
@@ -0,0 +1,42 @@
+package OrangeHRMTestCase;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import OrangeHRM_pages.Pages;
+
+public class TestCases {
+	WebDriver driver;
+	Pages li;
+	
+	@BeforeMethod
+	public void launchBrowser() {
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));}
+	
+	@Test
+	public void TestCases() {
+	Pages li=new Pages(driver);
+	li.enterURL();
+	li.enterUsername("Admin");
+	li.enterPassword("admin123");        
+	li.clickLogin();
+	//li.clickDashboard();
+	li.clickPIM();
+	li.clickEmployeeList();
+	li.enterEmployeeName("John smith Romer");
+	li.enterEmployeeID("0123");
+	li.clickSearch();
+	li.clickImage();
+	li.clickLogout();
+	
+	
+	}}
+			
+
